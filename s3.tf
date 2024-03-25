4e2a3489669ccb33889f257e5acec75aaf984d19
msatish1904 <99471611+msatish1904@users.noreply.github.com>
2024-03-25 20:30:33 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,3 @@
-resource "aws_s3_bucket" "s3" { 
-  bucket        = var.bucket_name
-  tags          = var.tags 
-}
-
 resource "aws_s3_bucket_versioning" "s3" {
   count  = var.enable_versioning ? 1 : 0
   bucket = aws_s3_bucket.s3.id
